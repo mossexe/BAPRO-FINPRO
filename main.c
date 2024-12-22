9278a6f2bcb2b2c4222148ff9eb77a28ce73e830
Jarrouu <129776094+tooOshiroo0o@users.noreply.github.com>
2024-12-22 17:07:56 +0700

    Health Database

--- main.c
+++ main.c
@@ -3,15 +3,10 @@
 #include <string.h>
 #include <stdlib.h>
 
-// this is for the  maximum number of diseases that can be stored
-#define MAX_DISEASES 100 
-
-
-void vaccineTracker(int ageInMonths);
-int ageCalculator();
-void HealthDatabase();
 int n, p,options;
 
+// this is for the  maximum number of diseases that can be stored
+#define MAX_DISEASES 100 
 
 typedef struct {
     char name[50];
@@ -23,229 +18,6 @@ typedef struct {
 Disease diseaseDatabase[MAX_DISEASES];
 int diseaseCount = 0;
 
-int main()
-{
-    int ageInMonths = ageCalculator(); // Calculate child's age in months
-    char menu;
-    do
-    {
-        printf("\nMenu Options:\n");
-        printf("A. Growth Tracker\n");
-        printf("B. Vaccine Tracker\n");
-        printf("C. Sleep Tracker\n");
-        printf("D. Health Database\n");
-        printf("E. Exit\n");
-        printf("Enter your choice: ");
-        scanf(" %c", &menu);
-
-        switch (menu)
-        {
-        case 'A':
-            break;
-
-        case 'B':
-            break;
-
-        case 'C':
-            break;
-
-        case 'D':
-            HealthDatabase();
-            break;
-
-        case 'E':
-            printf("Exiting program. Thank you!\n");
-            break;
-
-        default:
-            printf("Invalid Input! Try Again.\n");
-        }
-    } while (menu != 'E');
-
-    return 0;
-}
-
-// Function to calculate child's age in months
-int ageCalculator()
-{
-    int dates[2][3]; // Array to store birthdate [0][...] and current date [1][...]
-
-    // Get child's birthdate
-    printf("Enter child's birthdate (YYYY MM DD): ");
-    scanf("%d %d %d", &dates[0][0], &dates[0][1], &dates[0][2]);
-
-    // Get current date
-    time_t t = time(NULL);
-    struct tm currentDate = *localtime(&t);
-
-    dates[1][0] = currentDate.tm_year + 1900; // Current Year
-    dates[1][1] = currentDate.tm_mon + 1;     // Current Month
-    dates[1][2] = currentDate.tm_mday;        // Current Day
-
-    // Calculate age in months
-    int ageInYears = dates[1][0] - dates[0][0];
-    int ageInMonths = (dates[1][1] - dates[0][1]) + (ageInYears * 12);
-    if (dates[1][1] < dates[0][1] || (dates[1][1] == dates[0][1] && dates[1][2] < dates[0][2]))
-    {
-        ageInMonths -= 1; // Adjust if birthdate hasn't occurred yet this month
-    }
-    return ageInMonths;
-}
-
-// Function to calculate remaining months for a vaccine dosage
-int vaccineCalculator(int interval, int ageInMonths) {
-    int nextDose = interval - ageInMonths; // Remaining months until the dosage
-    return (nextDose > 0) ? nextDose : 0; // If overdue, set remaining to 0
-}
-
-// Vaccine Tracker function
-void vaccineTracker(int ageInMonths) {
-    char VaccineMenu;
-    int interval = 0;
-    int i;
-    int nextDose;
-
-    //Menu of upcoming vaccines based on different age groups
-    if (ageInMonths <= 6){
-        printf ("A. BCG/Polio\n");
-        printf ("B. Hepatitis B\n");
-        printf ("C. Rotavirus\n");
-        printf ("D. HiB\n");
-        scanf (" %c", &VaccineMenu);
-
-        switch (VaccineMenu){
-
-            case 'A':
-            printf ("-------- Polio/BCG -------\n");
-            printf ("Dosage | Due in   | Status\n");
-            interval = 2;
-            for (i = 0; i < 3; i ++){
-             nextDose = vaccineCalculator(interval, ageInMonths);
-             printf("%d      |%d Months  |   %c  \n",i+1, nextDose, nextDose == 0 ? 'V' : 'X');
-             interval +=2;              
-            }
-            break;
-
-            case 'B':
-            printf ("------- Hepatitis B ------\n");
-            printf ("Dosage | Due in   | Status\n");
-            interval = 1;
-            for (i = 0; i < 4; i ++){
-             nextDose = vaccineCalculator(interval, ageInMonths);
-             printf("%d      |%d Months  |   %c  \n",i+1, nextDose, nextDose == 0 ? 'V' : 'X');
-             interval ++;              
-            }
-            
-            break;
-            
-            case 'C':
-            printf ("-------- Rotavirus -------\n");
-            printf ("Dosage | Due in   | Status\n");
-
-            interval = 2;
-
-            for (i = 0; i < 3; i ++){
-             nextDose = vaccineCalculator(interval, ageInMonths);
-             printf("%d      |%d Months  |   %c  \n",i+1, nextDose, nextDose == 0 ? 'V' : 'X');
-             interval +=2;              
-            }
-            
-            break;
-            
-            case 'D':
-            printf ("----------- HiB ----------\n");
-            printf ("Dosage | Due in   | Status\n");
-
-            interval = 1;
-
-            for (i = 0; i < 3; i ++){
-             nextDose = vaccineCalculator(interval, ageInMonths);
-             printf("%d      |%d Months  |   %c  \n",i+1, nextDose, nextDose == 0 ? 'V' : 'X');
-             interval ++;              
-            }
-            
-            break;
-            
-            default:
-            printf ("Invalid Input! Try again!");
-            }
-    }
-
-    else if (ageInMonths > 6 && ageInMonths <= 12){
-        printf ("A. Influenza\n");
-        printf ("B. MMR\n");
-        scanf (" %c", &VaccineMenu);
-        
-        switch (VaccineMenu){
-            case 'A':
-            printf ("-------- Influenza -------\n");
-            printf ("Dosage | Due in   | Status\n");
-
-            interval = 12;
-
-            for (i = 0; i < 3 ; i ++){
-             nextDose = vaccineCalculator(interval, ageInMonths);
-             printf("%d      |%d Months  |   %c  \n",i+1, nextDose, nextDose == 0 ? 'V' : 'X');
-             interval ++;              
-            }
-            break;
-            
-            case 'B':
-            printf ("----------- MMR ----------\n");
-            printf ("Dosage | Due in   | Status\n");
-
-            interval = 9;
-
-            for (i = 0; i < 1; i ++){
-             nextDose = vaccineCalculator(interval, ageInMonths);
-             printf("%d      |%d Months  |   %c  \n",i+1, nextDose, nextDose == 0 ? 'V' : 'X');
-             interval ++;              
-            }
-
-            break;
-            
-            default:
-            printf ("Invalid Input! Try again!");
-        }
-
-    }
-    else if (ageInMonths > 12 && ageInMonths <= 24){
-        printf ("A. Hepatitis A\n");
-        printf ("B. Varicella\n");
-        scanf (" %c", &VaccineMenu);
-
-        switch (VaccineMenu){
-            case 'A':
-            printf ("------ Hepatitis A -------\n");
-            printf ("Dosage | Due in   | Status\n");
-
-            interval = 6;
-
-            for (i = 0; i < 2; i ++){
-             nextDose = vaccineCalculator(interval, ageInMonths);
-             printf("%d      |%d Months  |   %c  \n",i+1, nextDose, nextDose == 0 ? 'V' : 'X');
-             interval ++;              
-            }
-            break;
-            
-            case 'B':
-            printf ("-------- Varicella -------\n");
-            printf ("Dosage | Due in   | Status\n");
-
-            interval = 3;
-
-            for (i = 0; i < 2; i ++){
-             nextDose = vaccineCalculator(interval, ageInMonths);
-             printf("%d      |%d Months  |   %c  \n",i+1, nextDose, nextDose == 0 ? 'V' : 'X');
-             interval ++;              
-            }
-            break;
-            
-            default:
-            printf ("Invalid Input! Try again!");
-        }
-    }
-}
 void AddNewDisease() {
     if (diseaseCount >= MAX_DISEASES) {
         printf("\nThe disease database is full! Cannot add more diseases.\n");
@@ -418,4 +190,22 @@ void HealthDatabase () {
 
     }
     
+}
+int main() {
+    int choice;
+    while(1) {
+        printf("\nWelcome to the Baby Health Advisor!\n");
+        printf("Choose an option:\n");
+        printf("1. Learn about Baby Health\n");
+        printf("2. Add New Disease\n");
+        printf("3. View Diseases\n");
+        printf("4. Get Emergency Contact\n");
+        printf("5. Exit\n");
+        printf("Enter your choice: ");
+        
+        HealthDatabase();
+        
+        
+    }
+    return 0;
 }
