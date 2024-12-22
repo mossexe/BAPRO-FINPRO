6f481742c2f6e931fc3652d7534d5c423eb54496
mossexe <syifasarah1411@gmail.com>
2024-12-22 14:10:55 +0700

    Main menu function

--- main.c
+++ main.c
@@ -1,2 +1,41 @@
 #include <stdio.h>
-printf ("Hello World");
+
+int main()
+{
+
+    do
+    {
+        printf("\nMenu Options:\n");
+        printf("A. Growth Tracker\n");
+        printf("B. Vaccine Tracker\n");
+        printf("C. Sleep Tracker\n");
+        printf("D. Health Database\n");
+        printf("E. Exit\n");
+        printf("Enter your choice: ");
+        scanf(" %c", &menu);
+
+        switch (menu)
+        {
+        case 'A':
+            break;
+
+        case 'B':
+            break;
+
+        case 'C':
+            break;
+
+        case 'D':
+            break;
+
+        case 'E':
+            printf("Exiting program. Thank you!\n");
+            break;
+
+        default:
+            printf("Invalid Input! Try Again.\n");
+        }
+    } while (menu != 'E');
+
+    return 0;
+}
